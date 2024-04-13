5f08a9d5785cb0d1cb51cbef48ca29e4e8773fed
Eric Lampferhoff <lampthehoff@hotmail.com>
2024-04-13 21:57:32 +0200

    initial

---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+	import * as Avatar from '$lib/components/ui/avatar';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Button>Click me</Button>
+
+<Avatar.Root>
+	<Avatar.Fallback>CN</Avatar.Fallback>
+</Avatar.Root>
